9da8c39055720e6406844098b736a8eb246c8948
Alex <38843286+BeAllAround@users.noreply.github.com>
2021-06-05 00:34:37 +0200

    Add files via upload

--- Tests.cpp
+++ Tests.cpp
@@ -5,12 +5,14 @@
 #include<stdlib.h>
 #include<stdio.h>
 
+
 struct Node{
 	int*item{NULL};
 	struct Node*node{NULL};
 	Node()=default;
 };
 typedef struct Node Node;
+extern void show(Node*);
 
 void append(Node*&n, int*_item){
 	Node*&P = n;
@@ -31,17 +33,42 @@ void append(Node*&n, int*_item){
 	PLast->node->item = _item; // and assign new data
 }
 
+void remove(Node*&n, int _item){
+	Node*&P = n;
+	Node*Copy, *Shift{new Node}; // empty Node;
+
+	while(P!=NULL){
+		if(*(P->item) == _item){
+			Copy = P->node; // skip the node;
+			break;
+		}else append(Shift, P->item); // save the up-current;
+		P = P->node;
+	}
+	while(Copy!=NULL){
+		append(Shift, Copy->item);
+		Copy = Copy->node;
+	}
+
+	n = Shift; // override the existing variable;
+	return;
+}
+
 void show(Node*node){
-	Node*n = node;
-	printf("---show---\n");
+	Node*&n = node;
+	std::cout << "[ ";
 	while(n!=NULL){
-		printf("%d\n", *(n->item));
+		printf("%d, ", *(n->item));
 		n = n->node;
 	}
-	printf("---show---\n");
+	std::cout << "]";
 }
 
 
+
+std::ostream&operator<<(std::ostream&C, Node*&node){
+	show(node);
+	return C;
+}
 
 int main(){
 	Node*n = (struct Node*)malloc(sizeof(struct Node));
@@ -53,6 +80,17 @@ int main(){
 		append(n, new int(i));
 	}
 
-	show(n);
+	// tests
+	remove(n, 99);
+	remove(n, 0);
+	remove(n, 4);
+	remove(n, 1);
+	remove(n, 10);
+	remove(n, 22);
+	remove(n, 74);
+	remove(n, 51);
+	remove(n, 94);
+
+	std::cout << n << std::endl;
 	return 0;
 }
