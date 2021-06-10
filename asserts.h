5ae3e577d1e2415af7d5a9a6338c6291d5775745
Alex <38843286+BeAllAround@users.noreply.github.com>
2021-06-10 19:32:08 +0200

    Add files via upload

---
+++ asserts.h
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+#define TEST() void TESTS()
+
+extern void RUN();
+extern void TESTS();
+extern int main();
+
+void __attribute__((destructor)) RUN();
+
+
+void RUN(){
+        printf("Running Tests...\n");
+	TESTS();
+	main();
+}
+int main(){};
---
+++ home.cpp
@@ -0,0 +1,7 @@
+#include "./asserts.h"
+#include <iostream>
+
+
+TEST(){
+	std::cout << "RUNNER" <<std::endl;
+};
