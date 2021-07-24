502004b6289c7a67350c5cbf02471f5224ce2271
Alex <38843286+BeAllAround@users.noreply.github.com>
2021-07-24 17:08:27 +0200

    Add files via upload

--- Tests.js
+++ Tests.js
@@ -5,50 +5,35 @@ function removeElement(obj, elem){
 		for(var o of Object.keys(obj)){
 			if(o==elem){
 				delete obj[o]; // ((&Node)n = null);
-				return 1; // terminate further recursion
+				return true; // terminate further recursion
 			}
-			if(removeElement(obj[o], elem)==1)return 1;
+			if(removeElement(obj[o], elem))
+				return true;
 		}
 	}
-	return 0;
+	return false;
 }
-var obj = {want: "10", love: {me: "why", you: {e: {me: "AE"}}, ha: "HA",},};
-
-removeElement(obj, "me"); // or just -> while(removeElement(obj, "me"));
-console.log(obj);
-
-console.log(removeElement(obj, "me"));
-console.log(obj.love);
-
-
-// another example of erasing
-
-var list = [0, 1, 0, 1];
-delete list[0]; // null
-delete list[1]; // null
-list = list.filter(n=>n!=null);
-console.log(list);
 
 function findObject(obj, item, name){
-        if(name==item){
-                return 1;
-        }
+        if(name == item)
+		return true;
         if(obj instanceof Object){
                 for(var o in obj){
-                        if(findObject(obj[o], item, o)==1)return 1;
+                        if(findObject(obj[o], item, o))
+				return true;
                 }
         }
-        return 0;
+        return false;
 }
 
 function returnObject(obj, item, name, list){
-        if(name==item){
+        if(name == item){
                 list.push(obj);
                 return list;
         }
         if(obj instanceof Object){
                 for(var o in obj){
-                        if(findObject(obj[o], item, o)==1){
+                        if(findObject(obj[o], item, o)){
                                 list = returnObject(obj[o], item, o, list);
                         }
                 }
@@ -74,12 +59,33 @@ function return_object(obj, item){
 }
 */
 
-// another example of erasing
-console.log("---------------------");
-var obj2 = {want: 11, ha:{WHERE: 10}, love: {me: "why", you: {me: 401, ha: "DATA", e: {me: "AE"}}, ha: {data: "KEY"},}, me: "WE",};
-console.log(obj2);
-console.log(return_object(obj2, "e")); // [ { me: 'AE' } ]
-console.log(return_object(obj2, "ha")); // [ { WHERE: 10 }, 'DATA', { data: 'KEY' } ]
-console.log(return_object(obj2, "want")); // [ 11 ]
-console.log(return_object(obj2, "you")); // [ { me: 401, ha: 'DATA', e: { me: 'AE' } } ]
-console.log(return_object(obj2, "me")); // [ 'why', 401, 'AE', 'WE' ]
+function __main__(...args){
+	var obj = {want: "10", love: {me: "why", you: {e: {me: "AE"}}, ha: "HA",},};
+
+	removeElement(obj, "me"); // or just -> while(removeElement(obj, "me"));
+	console.log(obj);
+
+	console.log(removeElement(obj, "me"));
+	console.log(obj.love);
+
+
+	// another example of erasing
+
+	var list = [0, 1, 0, 1];
+	delete list[0]; // null
+	delete list[1]; // null
+	list = list.filter(n=>n!=null);
+	console.log(list);
+	
+	// another example of erasing
+	console.log("---------------------");
+	var obj2 = {want: 11, ha:{WHERE: 10}, love: {me: "why", you: {me: 401, ha: "DATA", e: {me: "AE"}}, ha: {data: "KEY"},}, me: "WE",};
+	console.log(obj2);
+	console.log(return_object(obj2, "e")); // [ { me: 'AE' } ]
+	console.log(return_object(obj2, "ha")); // [ { WHERE: 10 }, 'DATA', { data: 'KEY' } ]
+	console.log(return_object(obj2, "want")); // [ 11 ]
+	console.log(return_object(obj2, "you")); // [ { me: 401, ha: 'DATA', e: { me: 'AE' } } ]
+	console.log(return_object(obj2, "me")); // [ 'why', 401, 'AE', 'WE' ]
+}
+
+__main__();
