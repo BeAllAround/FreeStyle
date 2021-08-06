6c574416f875fc6a8bf65662ad721611e744a22d
Alex <38843286+BeAllAround@users.noreply.github.com>
2021-08-06 15:10:54 +0200

    Add files via upload

---
+++ Tests2.js
@@ -0,0 +1,21 @@
+function __searchObjects(obj, name, toFind, arr){
+	if(obj instanceof Object)
+		for(var i in obj)
+			arr = __searchObjects(obj[i], i, toFind, arr);
+	if(toFind == name){
+		arr.push({[`${toFind}`]: obj});
+		return arr;
+	}
+	return arr;
+}
+
+function searchObjects(obj, toFind){
+	return __searchObjects(obj, "", toFind, []);
+}
+
+(function __main__(){
+	var test_1 = {ha: 6, s: {ha: [7, 50, 100], s: {h: {ha: "TEA"}}}};
+	console.log(searchObjects(test_1, "s"));
+	console.log(searchObjects(test_1, "ha"));
+	console.log(searchObjects(test_1, "h"));
+})();
