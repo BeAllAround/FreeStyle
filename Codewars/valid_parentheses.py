e26856415f956f1c7ac60b8b8ebab40526961a0c
Alex <38843286+BeAllAround@users.noreply.github.com>
2021-08-13 16:56:27 +0200

    Add files via upload

---
+++ Codewars/valid_parentheses.py
@@ -0,0 +1,52 @@
+class Token:
+    def __init__(self, s):
+        self.s = s;
+        self.i = -1;
+        self.b = False;
+    def get(self):
+        if self.i == len(self.s)-1:
+            return False;
+        self.i += 1;
+        return True;
+    @property
+    def current(self):
+        return self.s[self.i];
+    @property
+    def lp(self):
+        return self.current == '(';
+    @property
+    def rp(self):
+        return self.current == ')';
+    
+def _valid(t):
+    while t.get():
+        if(t.lp):
+            try:
+                _valid(t);
+            except Exception:
+                if(not t.rp):
+                    raise Exception();
+            else:
+                raise Exception();
+        elif t.current == ')':
+            raise Exception();
+            
+def valid_parentheses(string):
+    if(string==''):
+        return True;
+    t = Token(string);
+    try:
+        _valid(t);
+    except Exception:
+        return False;
+    return True;
+
+if __name__ == "__main__":
+    print(valid_parentheses("dd()jtkr(urmf)i()d")); # True
+    print(valid_parentheses("(())(())")); # True
+    print(valid_parentheses("((()))")); # True
+    print(valid_parentheses("hi(hi)()")); # True;
+    print(valid_parentheses("((()))())")); # False
+    print(valid_parentheses(")()()")); # False
+    print(valid_parentheses("()()(")); # False
+    print(valid_parentheses("()())(")); # False
