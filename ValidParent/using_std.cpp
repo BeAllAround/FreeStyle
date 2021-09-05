724fb44a2372cdc15d5eca561b9276d1ae1679b3
Alex <38843286+BeAllAround@users.noreply.github.com>
2021-09-06 01:37:26 +0200

    Update using_std.cpp

--- ValidParent/using_std.cpp
+++ ValidParent/using_std.cpp
@@ -1,7 +1,67 @@
 #include <iostream>
 #include <sstream>
 #include <string>
+/*
+// codeWars source code
+#include <sstream>
 
+class Error{
+  public:
+    Error()=default;
+};
+
+class Error_2{
+  public:
+      Error_2()=default;
+};
+
+void _v(std::stringstream&iter){
+  bool b = false, b1 = false, b2 = false;
+  char chr;
+  while(iter >> chr){
+    if(chr == '('){
+      try{
+        _v(iter);
+        b = true;
+      }catch(int&err){
+        // do nothing
+      }
+      if(b)
+        throw 1;
+    }
+    else if(chr == ')')
+      throw 1;
+    else if(chr == '['){
+      try{
+        _v(iter);
+        b1 = true;
+      }catch(Error&err){};
+      if(b1)
+        throw Error();
+    }else if(chr == ']')
+      throw Error();
+    if(chr == '{'){
+      try{
+        _v(iter);
+        b2 = true; 
+      }catch(Error_2&err){};
+      if(b2)
+        throw Error_2();
+    }else if(chr == '}')
+      throw Error_2();
+  }
+}
+
+bool valid_braces(std::string str){
+  std::stringstream iter(str);
+  try{
+    _v(iter);
+  }catch(...){
+    return false;
+  }
+  return true;
+}
+*/
 void _v(std::stringstream&iter){
 	bool b = false;
 	char chr;
