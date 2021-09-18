53013c970dfa803140516d5ab8ed5d622f199aa5
Alex <38843286+BeAllAround@users.noreply.github.com>
2021-09-18 17:43:07 +0200

    Add files via upload

--- parsing.c
+++ parsing.c
@@ -8,29 +8,32 @@ int compareStr(char* str, char* rst)
 	int i;
 	if(strlen(str) != strlen(rst))
 		return 0;
-	for(i = 0; i < strlen(str); i++){
+	for(i = 0; i < strlen(str); i++)
 		if(str[i] != rst[i])
 			return 0;
-	}
 	return 1;
 }
 
-int _trim(char* _string, char* _match, int back, int forward)
+int _trim(char *_string, char *_match, int back, int forward)
 {
-	char* s = (char*)malloc(10000); // set the indefinite value
+	char *s = (char*)malloc(back + forward + 1); // set the indefinite value
 	int i, c = 0;
 
-	if(back > strlen(_string) || (back+forward) > strlen(_string) || !forward)
+	if(back > strlen(_string) || (back+forward) > strlen(_string) || !forward){
+		free(s);
 		return 0;
+	}
 
 	for(i = back; i < back+forward; i++)
 		s[c++] = _string[i];
 
-	s[++c] = 0;
-	return compareStr(s, _match);
+	s[c] = 0;
+	c = compareStr(s, _match);
+	free(s);
+	return c;
 }
 
-int strIndex(char* s, char* t)
+int strIndex(char *s, char *t)
 {
 	int i, t_l = strlen(t);
 	for(i = 0; s[i] != 0; i++)
@@ -43,22 +46,21 @@ typedef struct{
 	char* str;
 }Date;
 
-Date newDate(char*str)
+Date newDate(char *str)
 {
 	return(Date){str};
 }
 
-char* crop(char* str, int start, int end)
+char* crop(char *str, int start, int end)
 {
 	char* m = (char*)malloc(10000); // somewhat of a value;
 	int c = 0;
 	int i;
 	if(start >= end)
 		return"";
-	for(i=start; i < end; i++){
+	for(i = start; i < end; i++)
 		m[c++] = str[i];
-	}
-	m[c++] = 0;
+	m[c] = 0;
 	return m;
 }
 
@@ -78,16 +80,15 @@ char* parse(char* str, ...)
 		if(_trim(str, "%date", i, date)){
 			i += date - 1; 
 			_date = (((Date*)va_arg(argptr, void*))->str);
-			for(j=0; j<strlen(_date); j++){
+			for(j = 0; j < strlen(_date); j++)
 				data[l++] = _date[j];
-			}
 			continue;
 		}
 		data[l++] = str[i];
 
 	}
 	va_end(argptr);
-	data[++l] = 0;
+	data[l] = 0;
 	return data;
 }
 
@@ -95,9 +96,11 @@ char* parse(char* str, ...)
 int main(){
 	Date d = newDate("3/21/2021");
 	Date d1 = newDate("3/22/2021");
-	printf("%s", parse("TODAY: %date\nTOMORROW: %date\n", &d, &d1));
+	char *str;
+	printf("%s", str = parse("TODAY: %date\nTOMORROW: %date\n", &d, &d1));
 
 	printf("%d\n", strIndex("super", "superr"));
+	free(str);
 
 	return 0;
 }
