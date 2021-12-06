f371168310b75da3d9adde18413387743972eb49
Alex <38843286+BeAllAround@users.noreply.github.com>
2021-12-06 19:29:45 +0100

    Update Binary Tree.js

--- Binary Tree.js
+++ Binary Tree.js
@@ -25,3 +25,35 @@ function printTree(tree, n, b){
 
 // printTree({left: {left: {left: null, right:null, value: 5}, right: {left: null, right: null, value: 8}, value: 7},  right: {left: null, right: {left: null, right: null, value: 12}, value: 11}, value: 9}, 1, 1, 0);
 printTree({left: {left: {left: null, right:null, value: 5}, right: {left: null, right: null, value: 8}, value: 7},  right: {left: {left: null, right: null, value: 33}, right: {left: null, right: null, value: 30}, value: 11}, value: 9}, 0, 0);
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+const {stdout} = require('process');
+
+function _treeArray(tree, b, _list, __list){
+        if(tree == null)
+                return false;
+        if(__list instanceof Array)
+                __list.push(tree.value);
+        if(tree.left){
+                b = true;
+                __list.push([]);
+                _treeArray(tree.left, false, _list, __list[__list.length-1]);
+        }
+        if(tree.right){
+                if(!b)
+                        __list.push([]);
+                _treeArray(tree.right, false, _list, __list[__list.length-1]);
+        }
+
+
+}
+
+function treeArray(tree){
+        let l1 = [];
+        let l2 = l1;
+        _treeArray(tree, false, l1, l2);
+        return l1;
+}
+
+let obj = {left: {left: {left: null, right:null, value: 5}, right: {left: null, right: null, value: 8}, value: 7},  right: {left: {left: {left:null, right: null, value: 500}, right: null, value: 33}, right: {left: {left: null, right:null, value: 150}, right: {left:null, right: null, value: 100}, value: 30}, value: 11}, value: 9};
+console.log(treeArray(obj));
