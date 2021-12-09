3474e97fe834e763187eb7736027758ca12f224c
Alex <38843286+BeAllAround@users.noreply.github.com>
2021-12-09 18:46:42 +0100

    Update Sums of Parts.py

--- Codewars/Sums of Parts.py
+++ Codewars/Sums of Parts.py
@@ -1,22 +1,11 @@
-def isEmpty(arr):
-    return len(arr) == 0;
-
-def loop(arr, n):
-    if( n <= -1):
-        return [];
-    arr_1 = loop(arr, n-1);
-    arr_1.append(sum(arr[n:]));
-    return arr_1;
+from functools import reduce
 
 def parts_sums(ls):
-    # l_s = list(ls);
-    return(loop(ls, len(ls)));
-    # l = [ sum(ls[x:]) for x in range(0, len(ls))];
-    # l.append(0);
-    '''
-    while n < ls.__len__(): // significantly lower
-        l.append(sum(ls[n:]));
-        n+=1;
-    l.append(0);
-    '''
-    # return l;
+        if len(ls) == 0:
+	    return [0]
+    totalSum=reduce(lambda a, b: a + b, ls)
+    sumList=[totalSum]
+    for i in range(1,len(ls)+1):
+	totalSum-=ls[i-1]
+        sumList.append(totalSum)
+    return sumList
