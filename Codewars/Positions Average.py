9968fba6660e1196f575d5b2e73b69d637379141
Alex <38843286+BeAllAround@users.noreply.github.com>
2021-12-14 14:58:14 +0100

    Create Positions Average.py

---
+++ Codewars/Positions Average.py
@@ -0,0 +1,40 @@
+def pos_average(s):
+    s = s.split(', ');
+    n = 6 * len(s);
+    c = 0;
+    arr = [];
+    for x in range(0, len(s)):
+        item = s[x];
+        for j in range(x+1, len(s)):
+            item2 = (s[j]);
+            item = list(item);
+            item2 = list(item2);
+            last = 0;
+            for x1 in range(len(item)):
+                for j1 in range(last, len(item2)):
+                    if(item[x1] == item2[j1] and x1 == j1):
+                        last = j1+1;
+                        c+=1;
+                        break;
+            arr.append(c);
+            c = 0;
+
+    ''' # indivisual algorithm
+    item = s[3];
+    item2 = s[5];
+    print('--------------------------------');
+    item = list(item);
+    item2 = list(item2);
+    last = 0;
+    print(item, item2);
+    for x in range(len(item)):
+        for j in range(last, len(item2)):
+            if(item[x] == item2[j] and x == j):
+                # del item[x];
+                last = j+1;
+                # del item2[j];
+                c+=1;
+                break;
+    ''';
+    _final = (sum(arr) / (len(arr) * len(s[0]))) * 100;  # The calc!
+    return _final; # float(format(_final, '.10f'));
