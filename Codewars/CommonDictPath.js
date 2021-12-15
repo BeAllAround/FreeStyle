9cd9467a158951e676c1daf492127825d0f280db
Alex <38843286+BeAllAround@users.noreply.github.com>
2021-12-15 02:44:41 +0100

    Update CommonDictPath.js

--- Codewars/CommonDictPath.js
+++ Codewars/CommonDictPath.js
@@ -49,3 +49,33 @@ function getCommonDirectoryPath(pathes) {
                 _max = [item, item.length];
   return _max[0];
 }
+/* // reintegrated
+function getCommonDirectoryPath(pathes) {
+  arr = [];
+  check = '';
+  str_c = ['', Number.POSITIVE_INFINITY];
+  str = '';
+  let j, i; 
+  if((check = checker(pathes)) != '$'){
+    return check;
+  }
+  path = pathes[0];
+  for(j = 1; j < pathes.length; j++){
+    path1 = pathes[j];
+    c = 0, c1 = 0;
+    while(path[c] === path1[c1] && c < path.length){
+      str += path[c];
+      c++;
+      c1++;
+    } 
+    i = str.length-1;
+    while(str[i]!='/')
+          i--;
+    str = str.slice(0, i+1);
+    if(str.length < str_c[1])
+      str_c = [str, str.length];
+    str = '';
+  }
+  return str_c[0];
+}
+*/
