74a5d7ddebc1a4a844bd38c7ac3cd8f823f48f0d
Alex <38843286+BeAllAround@users.noreply.github.com>
2021-12-22 22:53:18 +0100

    Update Streams - endless arrays.js

--- Codewars/Streams - endless arrays.js
+++ Codewars/Streams - endless arrays.js
@@ -15,5 +15,39 @@ class Stream{
   tail(){
     return new Stream(this.op2(this._start), this.op1, this.op2);
   }
-  
 };
+
+/* // optimal solution
+class Stream{
+  constructor(start, op1, op2){
+    this._start = start;
+    this.op1 = op1;
+    this.op2 = op2;
+  }
+  
+  head(){
+    return this.op1(this._start);
+  }
+  
+  tail(){
+    return new Stream(this.op2(this._start), this.op1, this.op2);
+  }
+};
+// sample tests
+describe("Tests", () => {
+  it("test", () => {
+var increment = function(n) {return n + 1};
+var id = function(n) {return n};
+
+var naturalNumbers = new Stream(0, id, increment);
+Test.assertEquals(naturalNumbers.head(), 0)
+Test.assertEquals(naturalNumbers.tail().head(), 1)
+Test.assertEquals(naturalNumbers.tail().tail().head(), 2)
+
+var even = function(n) {return n * 2};
+var evenNumbers = new Stream(0, even, increment);
+Test.assertEquals(evenNumbers.head(), 0)
+Test.assertEquals(evenNumbers.tail().head(), 2)
+  });
+});
+*/
