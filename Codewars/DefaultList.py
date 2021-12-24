faaf1033f6418f453c864f68cd0263ed77897311
Alex <38843286+BeAllAround@users.noreply.github.com>
2021-12-24 18:30:39 +0100

    Update DefaultList.py

--- Codewars/DefaultList.py
+++ Codewars/DefaultList.py
@@ -6,6 +6,6 @@ class DefaultList(list):
         
     def __getitem__(self, key):
         try:
-            return super().__getitem__(key); # reuse to customize your class effectively - that's to say "without rewritting every single method." This is a great use of the "super" keyword.
+            return super().__getitem__(key); # reuse to customize your class effectively - that's to say "without rewriting every single method." This is a great use of the "super" keyword.
         except IndexError:
             return self.v;
