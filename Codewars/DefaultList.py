c6cc6233057be7f48e789584c99dac65c37a5ced
Alex <38843286+BeAllAround@users.noreply.github.com>
2021-12-24 18:29:55 +0100

    Create DefaultList.py

---
+++ Codewars/DefaultList.py
@@ -0,0 +1,11 @@
+class DefaultList(list):
+  
+    def __init__(self, arr, delf):
+        self.v = delf;
+        super().__init__(arr);
+        
+    def __getitem__(self, key):
+        try:
+            return super().__getitem__(key); # reuse to customize your class effectively - that's to say "without rewritting every single method." This is a great use of the "super" keyword.
+        except IndexError:
+            return self.v;
