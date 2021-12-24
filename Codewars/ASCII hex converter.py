8daf73aa0c4492283d5dd01a031234cf427c6336
Alex <38843286+BeAllAround@users.noreply.github.com>
2021-12-24 01:41:28 +0100

    Update ASCII hex converter.py

--- Codewars/ASCII hex converter.py
+++ Codewars/ASCII hex converter.py
@@ -15,15 +15,12 @@ class Converter():
         return ''.join([hex(ord(item))[2::] for item in s]);
 
     
-# or more suitable Python-based solution
+# or more suitable scripting Python-based solution
 '''
-  class Converter():
+class Converter():
     @staticmethod
     def to_ascii(h):
-        _s = '';
-        for x in range(0, len(h), 2):
-            _s += bytes.fromhex(h[x:x+2]).decode("ASCII");
-        return _s;
+        return ''.join([bytes.fromhex(h[x:x+2]).decode("ASCII") for x in range(0, len(h), 2)]);
     @staticmethod
     def to_hex(s):
         return ''.join([hex(ord(item))[2::] for item in s]);
