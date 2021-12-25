900dd8361a64eb77c14056b8603bcf8b60533fbc
Alex <38843286+BeAllAround@users.noreply.github.com>
2021-12-25 23:33:19 +0100

    Update Array Hyperrectangularity.py

--- Codewars/Array Hyperrectangularity.py
+++ Codewars/Array Hyperrectangularity.py
@@ -1,3 +1,25 @@
+def depth(arr, a, n):
+    if isinstance(arr, list):
+        n+=1;
+        a.append(n);
+        for item in arr:
+            a = depth(item, a, n);
+    return a;
+
+def check_depth(a1, a2):
+    return max(depth(a1, [], 0)) == max(depth(a2, [], 0));
+
+def flat(a1, a):
+    if isinstance(a1, list):
+        for item in a1:
+            a = flat(item, a);
+    else:
+        a.append(a1);
+    return a;
+
+def check_flat(a1, a2):
+    return len(flat(a1, [])) == len(flat(a2, []));
+
 def _loop(arr, a):
     if isinstance(arr, list):
         _s = 1;
@@ -11,12 +33,17 @@ def _loop(arr, a):
             if(type(start)!=type(item)):
                 return None;
             elif(isinstance(start, list) and isinstance(item, list)):
+                if(not check_flat(start, item)):
+                    return None;
+                if(not check_depth(start, item)):
+                    return None;
                 if(len(start)!=len(item)):
                     return None;
-            if(_loop(start, a)==None):
-                return None;
             if(_loop(item, a)==None):
                 return None;
+            if(_loop(start, a)==None):
+                return None;
+            
             start = item;
     return True;
 
@@ -24,7 +51,7 @@ def __loop(arr, a, index1, index2):
     if isinstance(arr, list):
         if(index2 not in a[0]):
             a.append(len(arr));
-        a[0].append(index2);
+            a[0].append(index2);
         for x in range(len(arr)):
             a = __loop(arr[x], a, index2, index2+1);
     return a;
@@ -35,3 +62,14 @@ def hyperrectangularity_properties(arr):
     if(_loop(arr, [])==None):
         return None;
     return tuple((__loop(arr, [[]], -1, 0))[1:]);
+
+'''
+# Codewars
+import numpy as np
+
+def hyperrectangularity_properties(arr):
+    try:
+        return np.array(arr, np.int32).shape
+    except:
+        return None
+'''
