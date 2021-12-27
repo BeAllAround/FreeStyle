bfe2d89db4649b25b79e7e360424994dbd091504
Alex <38843286+BeAllAround@users.noreply.github.com>
2021-12-27 02:26:40 +0100

    Update No adjacent integers sequence generator.py

--- Codewars/No adjacent integers sequence generator.py
+++ Codewars/No adjacent integers sequence generator.py
@@ -1,3 +1,34 @@
+# algorithm created and coded by Alex 
+
+def generate_sequence(lower, upper):
+    _list = (list(range(lower, upper)));
+    value = None;
+    pos = -1;
+    if(len(_list)%2 == 1):
+        pos = int(len(_list)/2);
+        value = _list[pos];
+        del _list[pos];
+    arr = [];
+    first, last = 0, len(_list)-1;
+    b = True;
+    while(first != (len(_list)/2)-1):
+        if(b):
+            arr.append(_list[last]);
+            arr.append(_list[first]);
+            b = False;
+        first+=1;
+        last-=1;
+        arr.append(_list[last]);
+        if(_list[first]+1 == arr[len(arr)-1]):
+            arr.insert(0, _list[first]);
+        else:
+            arr.append(_list[first]);
+    if(value != None):
+        arr.insert(0, value);
+    return arr;
+
+'''
+// bad performance
 def generate_sequence(lower, upper):
     _list = (list(range(lower, upper)));
     value = None;
@@ -25,3 +56,4 @@ def generate_sequence(lower, upper):
     if(value != None):
         arr.insert(pos, value);
     return arr;
+'''
