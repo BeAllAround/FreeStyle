3ac2c0317d5f69b60302886fedef9407852114c7
Alex <38843286+BeAllAround@users.noreply.github.com>
2022-01-01 08:31:27 +0100

    Update Crack the PIN.py

--- Codewars/Crack the PIN.py
+++ Codewars/Crack the PIN.py
@@ -4,11 +4,13 @@ def encrypt(_pass):
     return hashlib.md5(_pass.encode()).hexdigest();
 
 def search(hash, _s, n):
+    if n > 5:
+        return [False, '']
     if n == 5:
         if encrypt(_s) == hash:
             return [True, _s];
         else:
-            return [False, _s];
+            return [False, ''];
     for x in range(0, 10):
         v = search(hash, _s+str(x), n+1);
         if v[0]:
