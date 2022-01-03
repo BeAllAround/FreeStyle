d6aefe03cef608b765eaf1aaddba252b05fa5b09
Alex <38843286+BeAllAround@users.noreply.github.com>
2022-01-03 05:13:40 +0100

    Create Pair Zeros.py

---
+++ Codewars/Pair Zeros.py
@@ -0,0 +1,19 @@
+def search(arr, inx):
+    for x in range(inx+1, len(arr)):
+        if arr[x] == 0:
+            return x;
+    return -1;
+
+def pair_zeros(arr):
+    arr_1 = list(arr);
+    inx = -1;
+    x = 0;
+    modify_size = len(arr_1);
+    while(x < modify_size):
+        if(arr_1[x] == 0):
+            inx = search(arr_1, x)
+            if(inx!=-1):
+                del arr_1[inx];
+                modify_size = len(arr_1); # keep the fixed size
+        x+=1;
+    return arr_1;
