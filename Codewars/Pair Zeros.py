55853025ac5e29fc69e95f9eeb119bc67746b9f9
Alex <38843286+BeAllAround@users.noreply.github.com>
2022-01-03 05:23:53 +0100

    Update Pair Zeros.py

--- Codewars/Pair Zeros.py
+++ Codewars/Pair Zeros.py
@@ -17,3 +17,10 @@ def pair_zeros(arr):
                 modify_size = len(arr_1); # keep the fixed size
         x+=1;
     return arr_1;
+'''
+# Codewars
+from itertools import count
+def pair_zeros(arr, *args):
+    c = count(1)
+    return [elem for elem in arr if elem != 0 or next(c) % 2]
+'''
