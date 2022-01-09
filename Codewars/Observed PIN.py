da0d77295ffabdcab8cdcdcb691e9725b8a815b4
Alex <38843286+BeAllAround@users.noreply.github.com>
2022-01-09 14:55:34 +0100

    Update Observed PIN.py

--- Codewars/Observed PIN.py
+++ Codewars/Observed PIN.py
@@ -1,4 +1,4 @@
-def pop(arr):
+def shift(arr):
     try:
         return arr.pop(0);
     except IndexError:
@@ -6,17 +6,16 @@ def pop(arr):
     
 def produce(arr):
     l1 = [];
-    l2 = pop(arr);
-    l_ = pop(arr);
+    l2 = shift(arr);
+    l_ = shift(arr);
     while l_ != None:
         for item in l2:
             for jtem in l_:
                 l1.append(item+jtem)
         l2 = list(l1);
-        l_ = pop(arr);
-        if(l_ == None):
-            return l2;
-        l1 = []; ### l1.clear();
+        l_ = shift(arr);
+        l1.clear();
+    return l2;
         
 def get_pins(observed):
     pins = {1: [1,2, 4], 2: [1,2, 3, 5], 3: [3,2, 6], 4: [1, 4,5,7], 5: [2, 4,5, 6, 8], 6:[3, 5,6, 9], 7: [4,7, 8], 8: [5, 7,8, 9, 0], 9: [8, 6, 9], 0:[0,8]}
@@ -24,7 +23,7 @@ def get_pins(observed):
     for item in observed:
         arr.append([])
         arr[len(arr)-1].extend(list(map(str, pins[int(item)])));
-    use = (arr).pop(0);
+    use = shift(arr);
     if(len(arr) == 0):
         return use;
     return produce([use, *arr]);
