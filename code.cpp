b3625c1deebfe0ddf8c81e6ec2a5fabc5c975b0f
Alex <38843286+BeAllAround@users.noreply.github.com>
2022-01-10 09:15:18 +0100

    Add files via upload

---
+++ code.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+
+
+template<class T>
+std::vector<T> execute(std::vector<T>& v, T item){
+	v.push_back(item);
+	return v;
+}
+
+template<class T, class...Types>
+std::vector<T> execute(std::vector<T>& v, T item, Types...args){
+	v.push_back(item);
+	return execute(v, args...);	
+}
+
+template<class T, class...Types>
+std::vector<T> _execute(Types...args){
+	std::vector<T> v;
+	return execute(v, args...);
+}
+
+
+int main(){
+	// std::vector<int> v;
+	auto v = _execute<int>(1, 4, 5, 6068, 800);
+	for(auto item : v)
+		std::cout << item << std::endl;
+	return 0;
+}
