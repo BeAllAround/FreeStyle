c3e194b99ad9b42520363b492a7ba5d5c4b94bab
Alex <38843286+BeAllAround@users.noreply.github.com>
2022-01-18 17:53:33 +0100

    Update debug.h

--- asserts/debug/debug.h
+++ asserts/debug/debug.h
@@ -27,6 +27,9 @@ extern void __pause();
 
 #define RESET __c = 0
 
+#define start_time clock_t start = clock();
+#define end_time printf("[Cpu_time_used: %f]\n", ((double) (clock() - start)) / CLOCKS_PER_SEC);
+
 void __pause(){
 	char s[2];
 	_print("PAUSED");
