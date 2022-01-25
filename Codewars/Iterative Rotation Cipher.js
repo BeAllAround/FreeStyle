cfb025bec2bb8a35fd0298979ccb8f145e13b889
Alex <38843286+BeAllAround@users.noreply.github.com>
2022-01-25 21:42:43 +0100

    Update Iterative Rotation Cipher.js

--- Codewars/Iterative Rotation Cipher.js
+++ Codewars/Iterative Rotation Cipher.js
@@ -61,3 +61,24 @@ IterativeRotationCipher.decode = function(str){
     str = decode(num, str);
   return str;
 };
+
+/*
+let quote = `If you wish to make an apple pie from scratch, you must first invent the universe.`;
+let solution = `10 hu fmo a,ys vi utie mr snehn rni tvte .ysushou teI fwea pmapi apfrok rei tnocsclet`;
+IterativeRotationCipher.encode(10,quote) === solution; //true
+*/
+/* Step-by-step breakdown:
+Step 1 — remove all spaces:
+`Ifyouwishtomakeanapplepiefromscratch,youmustfirstinventtheuniverse.`
+
+Step 2 — shift the order of string characters to the right by 10:
+`euniverse.Ifyouwishtomakeanapplepiefromscratch,youmustfirstinventth`
+
+Step 3 — place the spaces back in their original positions:
+`eu niv erse .I fyou wi shtom ake anap plepiefr oms crat ch,yo umustf irs tinventth`
+
+Step 4 — shift the order of characters for each space-separated substring to the right by 10:
+`eu vni seer .I oufy wi shtom eak apan frplepie som atcr ch,yo ustfum sir htinventt`
+
+Repeat the steps 9 more times before returning the string with `10 ` prepended.
+*/
