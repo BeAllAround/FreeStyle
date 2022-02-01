a2b5d8c2892fd3417bb86c04ee8c8296aa7b3d8a
Alex <38843286+BeAllAround@users.noreply.github.com>
2022-02-01 14:49:36 +0100

    Update Simple number sequence.py

--- Codewars/Simple number sequence.py
+++ Codewars/Simple number sequence.py
@@ -12,6 +12,8 @@ def _find(arr):
     for j in arr[1:]:
         if(item+2 == j):
             arr1.append(item+1);
+        elif(j > item+3 or item+3 < j):
+            return -1
         item = j;
     if(len(arr1) == 0 or len(arr1) > 1):
         return -1;
@@ -19,25 +21,40 @@ def _find(arr):
     
 def missing(s):
     arr = [];
-    arr1 = []
+    arr1 = [];
     item = None
-    for x in range(1, 10000):
+    for x in range(1, 10):
         arr = list(_iter(s, x));
         if(check_sort(arr)):
-            break;
-    if len(arr) == 1 or len(arr) == 2:
-        _s = s;
-        b = False;
-        for x in range(1, 10010):
-            arr = list(_iter(s, x));
+            break
+    if(_find(arr) != -1):
+        return _find(arr)
+    else:
+        _s = s
+        b = False
+        for x in range(1, 10):
+            arr = list(_iter(s, x))
             for item in arr:
                 if(item.startswith('0')):
-                    s = (s[s.index('10'):len(s)])
+                    try:
+                        s = (s[s.index('10'):len(s)])
+                    except ValueError:
+                        return -1
                     b = True
-                    continue
+                    continue;
             if(check_sort(arr)):
                 break
+        b1 = False;
         if not b:
-            return 10
-        arr = (list(_iter(_s[0:_s.index(arr[0])], len(arr[0])-1))) + arr;
-    return _find(arr)
+            for x in range(1, len(_s)):
+                item = _s[x]
+                if(item.startswith('1')):
+                    arr = (list(_iter(_s[0:x], 1))) + list(_iter(_s[x:len(_s)], 2))
+                    b1 = True
+                    break
+        if not b1:
+            c = len(arr[0]) - 1
+            if(c == 0):
+                c = 1
+            arr = (list(_iter(_s[0:_s.index(arr[0])], c))) + arr
+    return _find(arr);
