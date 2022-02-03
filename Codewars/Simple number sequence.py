68fa293a58a13e10fe4e5ee3549041135227c210
Alex <38843286+BeAllAround@users.noreply.github.com>
2022-02-03 03:35:56 +0100

    Update Simple number sequence.py

--- Codewars/Simple number sequence.py
+++ Codewars/Simple number sequence.py
@@ -58,3 +58,29 @@ def missing(s):
                 c = 1
             arr = (list(_iter(_s[0:_s.index(arr[0])], c))) + arr
     return _find(arr)
+'''
+    # Codewars
+    def missing(seq):
+        for digits in range(1, len(seq) // 2 + 1):
+            my_seq = last = seq[:digits]
+            n = int(my_seq)
+            missing = None
+
+            while len(my_seq) < len(seq):
+                n += 1
+                my_seq += str(n)
+
+                if not seq.startswith(my_seq):
+                    if missing == None:
+                        missing = n
+                        my_seq = last
+                    else:
+                        break
+                else:
+                    last = my_seq
+
+            if my_seq == seq and missing:
+                return missing
+
+        return -1
+'''
