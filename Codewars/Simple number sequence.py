cdfd5b67b483d32e57e71eb7e471fba3aef036f1
Alex <38843286+BeAllAround@users.noreply.github.com>
2022-02-03 05:55:17 +0100

    Update Simple number sequence.py

--- Codewars/Simple number sequence.py
+++ Codewars/Simple number sequence.py
@@ -84,3 +84,117 @@ def missing(s):
 
         return -1
 '''
+
+
+'''
+def _iter(s, _split):
+    for x in range(0, len(s), _split):
+        yield (s[x : x+_split]);
+
+state = 0;
+
+def check_sort(arr):
+    return arr == sorted(arr);
+
+def _find(arr):
+    global state;
+    arr = list(map(int, arr))
+    try:
+        item = arr[0]
+    except IndexError:
+        return -1;
+    arr1 = []
+    for j in arr[1:]:
+        if(item+2 == j):
+            arr1.append(item+1);
+        elif(j > item+1 or j+1 < item or item == j):
+            return -1;
+        item = j;
+    if(len(arr1) == 0 or len(arr1) > 1):
+        if(len(arr1) == 0):
+            state = -2;
+        return -1;
+    return arr1[0]
+
+def __find(arr):
+    try:
+        item = arr[0];
+    except IndexError:
+        return True;
+    for j in arr[1:]:
+        if(item < j and j > item):
+            continue;
+        else:
+            return False;
+    return True;
+    
+def missing(s):
+    global state;
+    arr = []
+    arr1 = []
+    arr2 = [];
+    item = None;
+    # print(s);
+    for x in range(1, 10):
+        arr = list(_iter(s, x))
+        if(check_sort(arr)):
+            break
+    if(_find(arr) != -1):
+        return _find(arr)
+    else:
+        _s = s
+        b = False
+        _c = 1;
+        for x in range(1, 10):
+            arr = list(_iter(s, x))
+            for item in arr:
+                if(item.startswith('0')):
+                    try:
+                        c1 = _c;
+                        c2 = 2;
+                        # print("C1: ", c1, _c);
+                        while(s[0:s.index('1' + '0'*_c)+c2][-1] != '0'):
+                            c1+=1;
+                            c2+=1;
+                        s_1 = (s[0:s.index('1' + '0'*c1)]);
+                        s = (s[s.index('1' + '0'*c1):len(s)])
+                        # c = len(arr[0]) - 1
+                        __c = _c -1;
+                        # arr_2 = list(_iter(_s[0:s.index('1' + ('0' * __c))], __c));
+                        # if(c == 0):
+                            # c = 1
+                        # print(s_1, _c);
+                        arr2 = (list(_iter(s_1, c1)));
+                        arr = (list(_iter(s, c1)));
+                        # print(arr2, arr);
+                        __b=__find(arr2);
+                        if(__b):
+                            arr1.extend(arr2);
+                        _c += 1;
+                            
+                    except ValueError:
+                        pass;
+                        # return -1
+                    b = True
+                    continue;
+            if(check_sort(arr)):
+                break
+        b1 = False;
+        if not b:
+            for x in range(1, len(_s)):
+                item = _s[x]
+                if(item.startswith('1')):
+                    arr = (list(_iter(_s[0:x], 1))) + list(_iter(_s[x:len(_s)], 2))
+                    b1 = True
+                    break;
+        if not b1:
+            arr = arr1 + arr;
+            if(_find(arr) != -1):
+                print("D", arr);
+                return _find(arr);
+            c = len(arr[0]) - 1
+            if(c == 0):
+                c = 1
+            arr = (list(_iter(_s[0:_s.index(arr[0])], c))) + arr
+    return _find(arr)
+    '''
