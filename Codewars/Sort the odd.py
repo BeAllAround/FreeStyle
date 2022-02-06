69e02b953ff197ea38a15abd0318a32defa50a42
Alex <38843286+BeAllAround@users.noreply.github.com>
2022-02-06 04:58:36 +0100

    Create Sort the odd.py

---
+++ Codewars/Sort the odd.py
@@ -0,0 +1,10 @@
+def sort_array(source_array):
+    arr_even = []
+    _l = list(sorted(list(filter(lambda x: x%2 == 1, source_array))))
+    for x in range(len(source_array)):
+        item = source_array[x]
+        if item % 2 == 0:
+            arr_even.append([x, item])
+    for item in arr_even:
+        _l.insert(item[0], item[1])
+    return _l
