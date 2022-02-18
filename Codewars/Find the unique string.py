111f90c6175a06fefefaeba20443dafefa1d28d2
Alex <38843286+BeAllAround@users.noreply.github.com>
2022-02-18 02:11:15 +0100

    Create Find the unique string.py

---
+++ Codewars/Find the unique string.py
@@ -0,0 +1,35 @@
+def equal(item, item2):
+    c = 0;
+    if item == '':
+        return True;
+    for i in item:
+        if i in item2:
+            c += 1;
+    return c == len(item);
+
+
+def _f(s):
+    if s == '':
+        return ' ';
+    return s;
+
+def every(arr):
+    a = list(arr);
+    try:
+        item = a.pop(0);
+    except IndexError:
+        return True
+    for j in a:
+        if item != j:
+            return False;
+    return True;
+        
+def find_uniq(arr):
+    arr_c = list(map(_f, arr));
+    item = arr_c.pop(0);
+    if(every(arr_c)):
+        return item;
+    for j in arr_c:
+        if not equal(item.lower(), j.lower()):
+            return j;
+        item = j;
