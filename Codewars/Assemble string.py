a29c6b632979ebc956410ad9856c5936c7fd5cd4
Alex <38843286+BeAllAround@users.noreply.github.com>
2022-02-24 03:02:07 +0100

    Create Assemble string.py

---
+++ Codewars/Assemble string.py
@@ -0,0 +1,22 @@
+def assemble(input):
+    if(len(input) == 0):
+        return ''
+    arr = [*('*' * len(input[0]))]
+    _a = []
+    for item in input:
+        for x in range(len(arr)):
+            if item[x] != '*' and arr[x] == '*':
+                arr[x] = item[x]
+    return ''.join(arr).replace('*', '#')
+  
+'''
+    # Codewars
+    def assemble(input):
+      result = list(input[0]) if input else []
+
+      for i in input:
+          for j, k in enumerate(i):
+              result[j] = k if result[j] == '*' else result[j]
+
+      return ''.join(result).replace('*', '#')
+'''
