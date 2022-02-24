73792069f675a0ae9e9d7ee5570e0cecc9e89999
Alex <38843286+BeAllAround@users.noreply.github.com>
2022-02-24 04:32:23 +0100

    Update The last digit of a huge number.py

--- Codewars/The last digit of a huge number.py
+++ Codewars/The last digit of a huge number.py
@@ -28,4 +28,23 @@ def last_digit(lst):
 
   def last_digit(lst):
       return functools.reduce(powmod, lst[::-1], 1) % 10
+
+    def last_digit(lst):
+        if not lst:
+            return 1
+        else:
+            out = 1
+            for n in lst[len(lst):0:-1]:
+                out = n**out
+                if out > 2:
+                    out -= 2
+                    out %= 4
+                    out += 2
+        return lst[0]**out% 10
+        
+   def last_digit(lst):
+    n = 1
+    for x in reversed(lst):
+        n = x ** (n if n < 4 else n % 4 + 4)
+    return n % 10
 '''
