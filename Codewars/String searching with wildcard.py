b2ce5c6833c0ce044dbf6395372b5fc12e0761b8
Alex <38843286+BeAllAround@users.noreply.github.com>
2022-02-28 01:13:22 +0100

    Create String searching with wildcard.py

---
+++ Codewars/String searching with wildcard.py
@@ -0,0 +1,22 @@
+def find(needle, haystack):
+    s = ''
+    for x in range(len(haystack)):
+        for j in range(0, len(needle)):
+            if x+j < len(haystack):
+                if(needle[j] == haystack[x+j] or needle[j] == '_'):
+                    s += needle[j]
+        if s == needle:
+            return x
+        s = ''
+    return -1
+
+'''
+#Codewars
+import re
+
+def find(needle, haystack):
+    matched = re.search(re.escape(needle).replace('_', '.'), haystack)
+    if matched:
+        return matched.start()
+    return -1
+'''
