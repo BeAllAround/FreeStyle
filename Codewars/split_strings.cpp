f45943c9e2a19a7ba952e58f2ed360a9f0860d38
Alex <38843286+BeAllAround@users.noreply.github.com>
2022-03-14 22:56:10 +0100

    Update split_strings.cpp

--- Codewars/split_strings.cpp
+++ Codewars/split_strings.cpp
@@ -38,6 +38,59 @@ std::ostream&operator<<(std::ostream&o, std::vector<std::string>const v){
 	return o;
 }
 
+/*
+#include <string>
+#include <vector>
+
+std::string splice(std::string, unsigned long, unsigned long);
+std::string splice(std::string, unsigned long);
+
+std::string splice(std::string str, unsigned long start = 0){
+    return splice(str, start, str.length());
+}
+
+std::string splice(std::string str, unsigned long start, unsigned long end){
+    std::string _new;
+    unsigned long i(start);
+    while(str[i] != 0 && i < end)
+        _new += str[i++];
+    return _new;
+}
+
+
+std::vector<std::string> solution(const std::string &s)
+{
+  std::vector<std::string>v;
+  if(s == "")
+    return v;
+  for(unsigned long i = 0; i < s.length(); i+=2)
+    v.push_back(splice(s, i, i+2));
+  size_t size = v.size();
+  if(v[size-1].length() == 1)
+    v[size-1] = v[size-1] + "_";
+  return v;
+}
+
+// Codewars
+std::vector<std::string> solution(const std::string &s) {
+    std::vector <std::string> res;
+    for (int i = 0; i < s.length(); i += 2) res.push_back(s.substr(i, 2));
+    if (s.length() % 2) res[res.size() - 1] += "_";
+    return res;
+}
+
+#include <string>
+#include <vector>
+#include <regex>
+
+std::vector<std::string> solution(const std::string &s)
+{
+    std::regex r{".."};
+    auto str = s + '_';
+    return {std::sregex_token_iterator(str.begin(), str.end(), r), {}};
+}
+*/
+
 int main(){
 	auto v = solution("");
 	int t{};
