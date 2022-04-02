baad3bf1555f4ed71a3272f42a217951be492a79
Alex <38843286+BeAllAround@users.noreply.github.com>
2022-04-02 02:30:13 +0200

    Create Fibonacci, Tribonacci and friends.py

---
+++ Codewars/Fibonacci, Tribonacci and friends.py
@@ -0,0 +1,8 @@
+def Xbonacci(signature,n):
+    arr = signature[:n];
+    c = len(arr);
+    for _ in range(n-c):
+        arr.append(sum(arr[-c:])); # the last c elements
+    return arr;
+
+# rule => (n == |signature|)
