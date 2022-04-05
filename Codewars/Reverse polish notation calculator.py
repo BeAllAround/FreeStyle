7625b168be59d579b9dd3a31d0a4fd7218bebd7a
Alex <38843286+BeAllAround@users.noreply.github.com>
2022-04-05 09:06:39 +0200

    Update Reverse polish notation calculator.py

--- Codewars/Reverse polish notation calculator.py
+++ Codewars/Reverse polish notation calculator.py
@@ -1,49 +1,16 @@
 def calc(expr):
     arr = [];
+    opers = {'+': lambda x, y: x + y,
+             '-': lambda x, y: x - y,
+             '*': lambda x, y: x * y,
+             '/': lambda x, y: x / y
+            };
     for e in expr.split(' '):
         if '.' in e:
             arr.append(float(e));
         elif e.isdigit():
             arr.append(int(e));
-        else:
-            if e == '+':
-                arr[-2] = arr[-1] + arr[-2];
-                del arr[-1];
-            elif e == '*':
-                arr[-2] = arr[-1] * arr[-2];
-                del arr[-1];
-            elif e == '-':
-                arr[-2] = arr[-2] - arr[-1];
-                del arr[-1];
-            elif e == '/':
-                arr[-2] = int(arr[-2] / arr[-1]);
-                del arr[-1];
+        elif e in ['-', '+', '*', '/']:
+            arr.append(opers[e](arr.pop(len(arr)-2), arr.pop()));
     arr.append(0);
     return arr.pop(0);
-  
-'''
-import operator
-
-def calc(expr):
-    OPERATORS = {'+': operator.add, '-': operator.sub, '*': operator.mul, '/': operator.truediv}
-    stack = [0]
-    for token in expr.split(" "):
-        if token in OPERATORS:
-            op2, op1 = stack.pop(), stack.pop()
-            stack.append(OPERATORS[token](op1,op2))
-        elif token:
-            stack.append(float(token))
-    return stack.pop()
-
-operator = set(['+', '-', '*', '/'])
-
-def calc(expr):
-    stack = list()
-    for c in expr.split():
-        if c in operator : 
-            first = stack.pop()
-            second = stack.pop()
-            stack.append(str(eval(second + c + first)))
-        else : stack.append(c)
-    return eval(stack.pop()) if stack else 0
-'''
