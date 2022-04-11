da47993d60663ebb9b7202943e75268513659be2
Alex <38843286+BeAllAround@users.noreply.github.com>
2022-04-11 17:06:24 +0200

    Create fun with lists - reduce.js

---
+++ Codewars/Fun with lists/fun with lists - reduce.js
@@ -0,0 +1,20 @@
+function reduce(head, f, init) {
+  while(head){
+    init = f(init, head.data);
+    head = head.next;
+  }
+  return init;
+}
+
+/*
+function reduce(head, f, init) {
+  return head ? reduce(head.next, f, f(init, head.data)) : init;
+}
+function reduce(head, f, init) {
+  const _reduce = (node, accumulator) => {
+    return !node ? accumulator : _reduce(node.next, f(accumulator, node.data));
+  };
+  
+  return _reduce(head, init);
+}
+*/
