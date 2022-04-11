1c738bb39b88fc33577974b853d1af7d61583bd7
Alex <38843286+BeAllAround@users.noreply.github.com>
2022-04-11 08:24:28 +0200

    Update Same Array.py

--- Codewars/Same Array.py
+++ Codewars/Same Array.py
@@ -1,13 +1,17 @@
 from collections import Counter;
+from copy import deepcopy;
 
 def count(carr):
     return Counter(list(sorted(carr)));
     
 def same(arr_a, arr_b):
     c = 0;
+    arrb_c = deepcopy(arr_b);
     for j in arr_a:
-        for k in arr_b:
+        for x in range(len(arrb_c)):
+            k = arrb_c[x];
             if count(j) == count(k):
                 c += 1;
+                del arrb_c[x]; # .pop(x);
                 break;
     return c == len(arr_a) and c == len(arr_b);
