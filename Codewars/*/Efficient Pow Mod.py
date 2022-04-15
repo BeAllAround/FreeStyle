4ad885c8ff5f3f2dd7d5685e131ae3a683d98dc2
Alex <38843286+BeAllAround@users.noreply.github.com>
2022-04-16 00:01:33 +0200

    Create Efficient Pow Mod.py

---
+++ Codewars/*/Efficient Pow Mod.py
@@ -0,0 +1,11 @@
+def pow_mod(x, y, z):
+    number = 1
+    while y:
+        if y & 1:
+            number = number * x % z
+        y >>= 1
+        x = x * x % z
+    return number
+
+def power_mod(x, y, n):
+    return pow_mod(x,y,n);
