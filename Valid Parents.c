ad3e7912d87de60466b6645e4741cca13582f90f
Alex <38843286+BeAllAround@users.noreply.github.com>
2022-04-17 00:14:05 +0200

    Update Valid Parents.c

--- Valid Parents.c
+++ Valid Parents.c
@@ -1,7 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <assert.h>
 
+char * _slice(char *s){
+  int size = strlen(s), i; // cache so that you don't have to call it twice
+  char *stream, *m;
+  stream = m = malloc(size == 0 ? 0 : size);
+  for(i = 1; i < size; i++)
+    *stream++ = s[i];
+  *stream = 0;
+  return m;
+}
+
+int _v(char **str){
+  char c, *s;
+  while((c = *(*(str))++) != 0){
+    if(c == '('){
+      // s = _slice(*str);
+      // int b = _v(&s);
+      // free(s);
+      if(_v(str) != -1)
+        return -2;
+    }else if(c == ')')
+      return -1;
+  }
+  return 0;
+}
+
+bool validParentheses(char *str_in) {
+  return _v(&str_in) == 0;
+}
+
+/*
 int _v(char *str, int *c){
   char _c;
   ++*c;
@@ -20,3 +51,4 @@ bool validParentheses(char *str_in) {
   int c = -1;
   return _v(str_in, &c) == 0;
 }
+*/
