8c45ad312ae0180714d37bed9de3d534a82473a1
Alex <38843286+BeAllAround@users.noreply.github.com>
2022-04-17 18:57:40 +0200

    Update Twisted Sum.c

--- Codewars/Twisted Sum.c
+++ Codewars/Twisted Sum.c
@@ -5,6 +5,14 @@ int digits(int n){
   return d;
 }
 
+/*
+int digits(int num){
+  int s, pow;
+  for(s = 0, pow = 1; pow <= num; s += (num/pow)%10, pow*= 10);
+  return s;
+}
+*/
+
 int compute_sum(int n) {
   int i, s = 0;
   for(i = 0; i <= n; i++)
