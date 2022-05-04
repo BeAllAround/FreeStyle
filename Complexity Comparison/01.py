6ae63ebeca825200ac24736c9ffa3290bf2208d7
Alex <38843286+BeAllAround@users.noreply.github.com>
2022-05-04 23:02:55 +0200

    Add files via upload

--- Complexity Comparison/01.py
+++ Complexity Comparison/01.py
@@ -21,10 +21,52 @@ class _Node:
 
     def _append(self, value): # O(n);
         n = _Node(value);
-        pre = self.tail.v;
+        pre = _Node(self.tail.v); #
         self.tail.next = n;
         self.tail = self.tail.next;
         self.tail.previous = pre;
+
+    def remove(self, value):
+        head = self;
+        if head and head.v == value:
+            self = self.next;
+            self.previous = None;
+            if not self:
+                return None;
+            tail = self;
+            while tail:
+                if tail.next == None:
+                    break;
+                tail = tail.next;
+            self.tail = tail;
+            return self;
+
+        while head != None:
+            if head.next:
+                if head.next.v == value:
+                    break;
+            head = head.next;
+        if not head:
+            return self;
+
+        if head.next.next:
+            head.next.next.previous = head.next.previous;
+            head.next = head.next.next;
+        else:
+            head.next = None;
+
+        while head:
+            if head.next == None:
+                self.tail = head;
+            head = head.next;
+        while head:
+            if head.previous == None:
+                self.next = head;
+                break;
+            else:
+                head = head.previous;
+
+        return self;
 
     def __repr__(self):
         n = self;
@@ -32,7 +74,7 @@ class _Node:
         while n != None:
             arr.append(n.v);
             n = n.next;
-        return arr.__repr__();
+        return '{' + arr.__repr__() + '}';
     
     def toObject(self):
         obj = {};
@@ -52,8 +94,28 @@ class _Node:
 
 start = time();
 n = _Node(0);
-for x in range(1, 10000):
+for x in range(1, 10):
     n._append(x);
 print(n);
+n = n.remove(0);
+n = n.remove(9);
+n = n.remove(2);
+n = n.remove(3);
+n = n.remove(5);
+n = n.remove(6);
+n = n.remove(7);
+n.append(101);
+n = n.remove(101);
+n = n.remove(101);
+n._append(101);
+n._append(101);
+n._append(104);
+n = n.remove(104);
+n._append(107);
+n = n.remove(107);
+n = n.remove(101);
+n = n.remove(1);
+print('AFTER: ', n.toObject());
+# print(n.toObject());
 # print(n.toObject());
 print(time() - start);
