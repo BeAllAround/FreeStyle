e51c826f14b87ee01bcc384148025486f09a6f44
Alex <38843286+BeAllAround@users.noreply.github.com>
2022-06-23 21:46:46 +0200

    Update _format.js

--- _format.js
+++ _format.js
@@ -1,24 +1,25 @@
 const util = require('util');
 
-function forward(s, start, l){
-	let _ = '';
-	for(let i = start; i < start+l && i < s.length; i++)
-		_ += s[i];
-	return _;
+function forward(s, start, l, toMatch){
+        let _ = '';
+        for(let i = start, j = 0; i < start+l && i < s.length && j < toMatch.length; i++, j++)
+                if(s[i] != toMatch[j])
+                        return false;
+        return true;
 }
 
 function _format(s, ...args){
-	let _s = '';
-	let i, j = 0;
-	for(i = 0; i < s.length; i++){
-		if(forward(s, i, 2) == '%d' && j < args.length){
-			_s += args[j++].toString();
-			i += 2-1; // (length-1);
-		}else{
-			_s += s[i];
-		}
-	}
-	return _s;
+        let _s = '';
+        let i, j = 0;
+        for(i = 0; i < s.length; i++){
+                if(forward(s, i, 2, '%d') && j < args.length){
+                        _s += args[j++].toString();
+                        i += 2-1; // (length-1);
+                }else{
+                        _s += s[i];
+                }
+        }
+        return _s;
 }
 
-util.log(_format("((%d super %d %d %d))", 1, 44, 11, 101));
+util.log(_format("((%d super %d %d %d %%d))", 1, 44, 11, 101, 88));
